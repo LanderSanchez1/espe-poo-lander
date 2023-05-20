7cda7f72eaf2bf95c5a5bd700ae4093ff7abf917
Lánder Sánchez <132688613+LanderSanchez1@users.noreply.github.com>
2023-05-19 23:05:17 -0500

    Update POO_S3_L1_P5_MetodosParametrosArgumentos.java

--- POO_S3_L1_P5_MetodosParametrosArgumentos/src/main/java/com/mycompany/poo_s3_l1_p5_metodosparametrosargumentos/POO_S3_L1_P5_MetodosParametrosArgumentos.java
+++ POO_S3_L1_P5_MetodosParametrosArgumentos/src/main/java/com/mycompany/poo_s3_l1_p5_metodosparametrosargumentos/POO_S3_L1_P5_MetodosParametrosArgumentos.java
@@ -4,16 +4,13 @@
 
 package com.mycompany.poo_s3_l1_p5_metodosparametrosargumentos;
 import javax.swing.JOptionPane;
-/**
- *
- * @author User
- */
+
 public class POO_S3_L1_P5_MetodosParametrosArgumentos {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
          Operaciones op1 = new Operaciones();
-        //Se pide al usuario dos numeros
+        //Se pide dos numeros
         int num1 = Integer.parseInt(JOptionPane.showInputDialog("Deme el primer numero"));
         int num2 = Integer.parseInt(JOptionPane.showInputDialog("Deme el segundo numero"));
         op1.sumar(num1, num2);
