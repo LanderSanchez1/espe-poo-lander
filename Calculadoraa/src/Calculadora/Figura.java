0ceeb3647205d2c571b6409c9eb22a17b2419634
Lánder Sánchez <132688613+LanderSanchez1@users.noreply.github.com>
2023-06-16 23:32:51 -0500

    Add files via upload

---
+++ Calculadoraa/src/Calculadora/Figura.java
@@ -0,0 +1,10 @@
+package Calculadora;
+
+//Lander Sanchez
+	import java.util.Scanner;
+
+	abstract class Figura {
+	    public abstract void calcularArea();// Método abstracto para calcular el área de la figura
+	    public abstract void calcularPerimetro(); //Método abstracto para calcular el perimetro de la figura
+	}
+
