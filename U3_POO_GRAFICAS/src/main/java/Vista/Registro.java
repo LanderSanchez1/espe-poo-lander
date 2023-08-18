d3d22f0f622b346a6ad2e577f4d18114cb2a1bf4
Lánder Sánchez <132688613+LanderSanchez1@users.noreply.github.com>
2023-08-18 14:13:17 -0500

    prueba/lab 2
    prueba/lab 2

--- U3_POO_GRAFICAS/src/main/java/Vista/Registro.java
+++ U3_POO_GRAFICAS/src/main/java/Vista/Registro.java
@@ -1,135 +1,139 @@
-
 package Vista;
 
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
 
 public class Registro extends JFrame {
-//creamos una venana grafica con JFrame    
-    
+
     public JPanel panel;
-    public Registro(){
-    this.setSize(500,500);
-    setTitle("REGISTRO");
-   
-    //Para que cierre el sistema
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    
-    
-    //Centrar la ventana
-    //setBounds(500, 200, 500, 500);
-    
-    //centrar pantalla
-    setLocationRelativeTo(null);
-    iniciarComponentes();
-    
-    }  
-    
-    private void iniciarComponentes(){
+    private ButtonGroup estadoGroup;
+
+    public Registro() {
+        this.setSize(600, 600);
+        setTitle("REGISTRO");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        iniciarComponentes();
+    }
+
+    private void iniciarComponentes() {
         colocarPanel();
         colocarEtiquetas();
         colocarBotones();
         colocarCajatexto();
-        colocarAreaTexto();
+        colocarTabla();
     }
-    private void colocarPanel(){
-        panel=new JPanel();
-    
-        
-        //damos color al panel
-         panel.setBackground(Color.GREEN);     
-         
-        //poner el panel sobre la ventana registro
-         this.getContentPane().add(panel);
-    }
-    //crear etiquetas
-    public void colocarEtiquetas(){
-        JLabel label=new JLabel("REGISTRAR", SwingConstants.CENTER);
-        panel.add(label);
+
+    private void colocarPanel() {
+        panel = new JPanel();
+        panel.setBackground(Color.GREEN);
         panel.setLayout(null);
-        label.setBounds(100,10, 350, 50);
-        label.setForeground(Color.BLUE);
-        label.setBackground(Color.ORANGE);
-        label.setOpaque(true);
-        
-        //creamos objeto
-                JLabel label1=new JLabel();
-                label.setText("Nombre");
-                
-                //Agregamos la etiqueta al panel
-                panel.add(label1);
-                
-                //Ubicar
-                label1.setHorizontalAlignment(HEIGHT);
-                
-                //Fuente
-                label1.setForeground(Color.red);
-                label1.setFont(new Font("arial", Font.PLAIN, 20));
-                label1.setBounds(80,70, 350, 50);
-                
-                //poner imagen
-                JLabel labelimagen=new JLabel();
-                ImageIcon imagen = new ImageIcon("registro.png");
-                labelimagen.setBounds(30,110, 100, 50);
-                panel.add(labelimagen);
-                
-                labelimagen.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(labelimagen.getWidth(), labelimagen.getHeight(), Image.SCALE_SMOOTH)));
-                      
+        this.getContentPane().add(panel);
     }
-    public void colocarBotones(){
-        JButton boton1=new JButton("CLICK");
-        boton1.setBounds(30, 310,100 ,50);
-        panel.add(boton1);
-        boton1.setEnabled(true);
-        boton1.setMnemonic('a');
-        
-        boton1.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 20, true));
-        JButton boton2=new JButton("imagen");
-        ImageIcon imagen1=new ImageIcon("boton.png");
-        
-        boton2.setBounds(210,310, 100, 50);
-        panel.add(boton2);
-        boton2.setEnabled(true);
-        boton2.setMnemonic('b');
-        
-        boton1.setIcon(new ImageIcon(imagen1.getImage().getScaledInstance(boton1.getWidth(),boton1.getHeight(),Image.SCALE_SMOOTH)));
-                
-                
+
+    public void colocarEtiquetas() {
+        JLabel labelEstudiante = new JLabel("Estudiante:");
+        labelEstudiante.setBounds(30, 10, 100, 30);
+        panel.add(labelEstudiante);
+
+        JLabel[] etiquetas = {
+            new JLabel("Código:"),
+            new JLabel("Cedula:"),
+            new JLabel("Apellido:"),
+            new JLabel("Nombre:"),
+            new JLabel("Mail:"),
+            new JLabel("Teléfono:"),
+            new JLabel("Dirección:"),
+            new JLabel("Materia:")
+        };
+
+        for (int i = 0; i < etiquetas.length; i++) {
+            etiquetas[i].setBounds(30, 50 + i * 40, 100, 30);
+            panel.add(etiquetas[i]);
+        }
+
+        JLabel labelEstado = new JLabel("Estado:");
+        labelEstado.setBounds(30, 410, 100, 30);
+        panel.add(labelEstado);
+
+        estadoGroup = new ButtonGroup();
+        JRadioButton radioButtonActivo = new JRadioButton("Activo");
+        radioButtonActivo.setBounds(140, 410, 80, 30);
+        panel.add(radioButtonActivo);
+        estadoGroup.add(radioButtonActivo);
+
+        JRadioButton radioButtonInactivo = new JRadioButton("Inactivo");
+        radioButtonInactivo.setBounds(220, 410, 80, 30);
+        panel.add(radioButtonInactivo);
+        estadoGroup.add(radioButtonInactivo);
     }
-    public void colocarCajatexto(){
-        JTextField cajaTexto1=new JTextField();
-        cajaTexto1.setBounds(130, 80, 250, 30);
-        panel.add(cajaTexto1);
-        cajaTexto1.setText("POO");
-        System.out.println("el texto de la caja es: " +cajaTexto1.getText());
-        
+
+    public void colocarBotones() {
+        String[] botonesText = {"Nuevo", "Guardar", "Editar", "Eliminar", "Salir"};
+        for (int i = 0; i < botonesText.length; i++) {
+            JButton boton = new JButton(botonesText[i]);
+            boton.setBounds(30 + i * 100, 460, 90, 30);
+            panel.add(boton);
+        }
     }
-    public void colocarAreaTexto(){
-        JTextArea textarea1=new JTextArea();
-        textarea1.setBounds(130, 130, 250, 150);
+
+    public void colocarCajatexto() {
+        JTextField cajaTextoCodigo = new JTextField();
+        cajaTextoCodigo.setBounds(140, 50, 200, 30);
+        panel.add(cajaTextoCodigo);
+
+        JTextField[] cajasTexto = new JTextField[5];
+        for (int i = 0; i < cajasTexto.length; i++) {
+            cajasTexto[i] = new JTextField();
+            cajasTexto[i].setBounds(140, 90 + i * 40, 200, 30);
+            panel.add(cajasTexto[i]);
+        }
+
+        String[] opcionesMateria = {"POO", "Computacion digital", "Edo", "ingles"};
+        JComboBox<String> comboBoxMateria = new JComboBox<>(opcionesMateria);
+        comboBoxMateria.setBounds(140, 330, 200, 30);
+        panel.add(comboBoxMateria);
+    }
+
+    public void colocarAreaTexto() {
+        JTextArea textarea1 = new JTextArea();
+        textarea1.setBounds(140, 450, 200, 60);
         panel.add(textarea1);
-        textarea1.setText("POO");
-        textarea1.append("\n Escribir aqui....");
-        
-        JScrollPane barra=new JScrollPane(textarea1);
+
+        JScrollPane barra = new JScrollPane(textarea1);
         barra.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        
         barra.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        barra.setBounds(130, 130, 250, 150);
+        barra.setBounds(140, 450, 200, 60);
         panel.add(barra);
-        
+    }
+
+    private void colocarTabla() {
+        String[] columnNames = {"Codigo", "Cedula", "Apellido", "Nombre", "Mail", "Telefono", "Direccion", "Materia", "Estado"};
+
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+        JTable tabla = new JTable(model);
+
+        JScrollPane scrollPane = new JScrollPane(tabla);
+        scrollPane.setBounds(400, 200, 500, 150);
+        panel.add(scrollPane);
     }
 }
+
+
