af4767baec4e0b1bf07a32610a59fd68682c2696
Lánder Sánchez <132688613+LanderSanchez1@users.noreply.github.com>
2023-08-28 23:55:47 -0500

    Practica POO1 variables
    Practica POO1 variables

---
+++ Practica POO1 variables/src/Practica/poo1/variables/PracticaPOO1Variables.java
@@ -0,0 +1,42 @@
+
+package Practica.poo1.variables;
+
+/**
+ *
+ * @author Lander Sanchez
+ */
+public class PracticaPOO1Variables {
+
+    /**
+     * Byte
+     * Short
+     * Int
+     * Long
+     * Char
+     * double
+     * float
+     * string
+     * boolean
+     */
+    public static void main(String[] args) {
+        //numeros enteros
+        byte a=1;
+        short b=20;
+        int c=100000;
+        long d=30;
+        
+        //caracter
+        char e='h';
+        
+        double f=4.5;
+        float g=4.56f;
+        
+        String h = "Hola Mundo";
+        
+        boolean i = 5>10;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
