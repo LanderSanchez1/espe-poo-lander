b0de23fc31bd700a453f4387545f9afe9d7e2f32
Lánder Sánchez <132688613+LanderSanchez1@users.noreply.github.com>
2023-08-29 00:05:46 -0500

    POO_Graficasss
    POO_Graficasss

---
+++ POO_Graficasss/src/poo_graficas/POO_Graficas.java
@@ -0,0 +1,12 @@
+package poo_graficas;
+
+import vista.Registro;
+
+public class POO_Graficas {
+
+    public static void main(String[] args) {
+        Registro regis = new Registro();
+        regis.setVisible(true);
+    }
+    
+}
