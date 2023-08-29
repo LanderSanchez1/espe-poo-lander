38698c809621be6221fdb517ff90ede86251ca0c
Lánder Sánchez <132688613+LanderSanchez1@users.noreply.github.com>
2023-08-28 23:53:36 -0500

    POO Operadores relacionales
    POO Operadores relacionales

---
+++ POO Operadores relacionales/src/Poo/operadores/relacionales/POOperadoresRelacionales.java
@@ -0,0 +1,25 @@
+
+package Poo.operadores.relacionales;
+
+/**
+ *
+ * @author lANDER sANCHEZ
+ */
+public class POOperadoresRelacionales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int val1= 10;
+        int val2= 4;
+        System.out.println(val1==val2); //false
+        System.out.println(val1!=val2); //true
+        System.out.println(val1>val2); //true
+        System.out.println(val1<val2); //false
+        System.out.println(val1>=val2); //true
+        System.out.println(val1<=val2); //false
+        
+    }
+    
+}
