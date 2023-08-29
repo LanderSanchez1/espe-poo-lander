d6eeeb868bdbe2602dacfdad7885be160099c939
Lánder Sánchez <132688613+LanderSanchez1@users.noreply.github.com>
2023-08-28 23:52:49 -0500

    POO Operadores
    POO Operadores

---
+++ POO Operadores/src/Poo/operadores/POOOperadores.java
@@ -0,0 +1,39 @@
+
+package Poo.operadores;
+
+/**
+ *
+ * @author Lander Sanchez
+ */
+public class POOOperadores {
+    
+    public static void main(String[] args) {
+        //Numeros
+        int a=1;
+        int b=7;
+        
+        //Suma
+        int suma = a+b;
+        int resta=a-b;
+        int multiplicacion=a*b;
+        int division=a/b;
+
+        System.out.println("Primer numero: ");
+        System.out.println(a);
+        System.out.println("Primer numero +1: ");
+        System.out.println(++a);
+        System.out.println("Segundo numero: ");
+        System.out.println(b);
+        System.out.println("Segundo numero +1: ");
+        System.out.println(++b);
+        System.out.println("Suma de los dos numeros: ");
+        System.out.println(suma);
+        System.out.println("Resta de dos numeros: ");
+        System.out.println(resta);
+        System.out.println("Multiplicacion de dos numeros: ");
+        System.out.println(multiplicacion);
+        System.out.println("Division de dos numeros: ");
+        System.out.println(division);
+    }
+    
+}
